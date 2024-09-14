da6fe3583e7872ca9aa62e1dc0aa72279c8bde03
Marvin Scholz <epirat07@gmail.com>
2024-09-14 03:45:56 +0200

    lavfi/afir_template: fix variable shadowing

--- libavfilter/afir_template.c
+++ libavfilter/afir_template.c
@@ -197,7 +197,7 @@ static int fn(fir_quantum)(AVFilterContext *ctx, AVFrame *out, int ch, int ioffs
             const int input_partition = j;
             const int coeff_partition = i;
             const int coffset = coeff_partition * seg->coeff_size;
-            const ftype *blockout = (const ftype *)seg->blockout->extended_data[ch] + input_partition * seg->block_size;
+            const ftype *blockout2 = (const ftype *)seg->blockout->extended_data[ch] + input_partition * seg->block_size;
             const ctype *coeff = ((const ctype *)seg->coeff->extended_data[ch]) + coffset;
 
             if (j == 0)
@@ -205,9 +205,9 @@ static int fn(fir_quantum)(AVFilterContext *ctx, AVFrame *out, int ch, int ioffs
             j--;
 
 #if DEPTH == 32
-            s->afirdsp.fcmul_add(sumin, blockout, (const ftype *)coeff, part_size);
+            s->afirdsp.fcmul_add(sumin, blockout2, (const ftype *)coeff, part_size);
 #else
-            s->afirdsp.dcmul_add(sumin, blockout, (const ftype *)coeff, part_size);
+            s->afirdsp.dcmul_add(sumin, blockout2, (const ftype *)coeff, part_size);
 #endif
         }
 
