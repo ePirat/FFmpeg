8dfaa8366a2948a7cbda5c87dc5ffbafa629ec53
Marvin Scholz <epirat07@gmail.com>
2024-09-15 01:51:24 +0200

    lavfi/avf_showvolume: fix variable shadowing

--- libavfilter/avf_showvolume.c
+++ libavfilter/avf_showvolume.c
@@ -418,12 +418,12 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *insamples)
     /* draw channel names */
     for (c = 0; c < inlink->ch_layout.nb_channels && s->h >= 10 && s->draw_text; c++) {
         if (s->orientation) { /* vertical */
-            int ret = av_channel_name(channel_name, sizeof(channel_name), av_channel_layout_channel_from_index(&inlink->ch_layout, c));
+            ret = av_channel_name(channel_name, sizeof(channel_name), av_channel_layout_channel_from_index(&inlink->ch_layout, c));
             if (ret < 0)
                 continue;
             drawtext(out, c * (s->h + s->b) + (s->h - 10) / 2, outlink->h - 35, channel_name, 1);
         } else { /* horizontal */
-            int ret = av_channel_name(channel_name, sizeof(channel_name), av_channel_layout_channel_from_index(&inlink->ch_layout, c));
+            ret = av_channel_name(channel_name, sizeof(channel_name), av_channel_layout_channel_from_index(&inlink->ch_layout, c));
             if (ret < 0)
                 continue;
             drawtext(out, 2, c * (s->h + s->b) + (s->h - 8) / 2, channel_name, 0);
